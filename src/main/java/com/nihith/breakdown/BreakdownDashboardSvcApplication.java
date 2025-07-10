2de6f2e496c2aa7608f367dbf1adcb3aa7efa9d9
sistla_n <nihithsistla98@gmail.com>
2025-07-10 20:27:53 +0530

    Process Settlements Algorithm

--- src/main/java/com/nihith/breakdown/BreakdownDashboardSvcApplication.java
+++ src/main/java/com/nihith/breakdown/BreakdownDashboardSvcApplication.java
@@ -2,8 +2,11 @@ package com.nihith.breakdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.nihith"})
 public class BreakdownDashboardSvcApplication {
 
 	public static void main(String[] args) {
